c944a3a6abc4ee2746d07a8c7fc62f12032155a9
Shreyans Kothari <sk1997@bu.edu>
2020-07-19 12:59:14 +0530

    Commiting my first file to git - a script with a few useful packages

---
+++ data-science-professional-certificate.R
@@ -0,0 +1,14 @@
+install.packages("dslabs")
+library(dslabs)
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("ggplot2")
+library(ggplot2)
+install.packages("rafalib")
+library(rafalib)
+install.packages("dplyr")
+library(dplyr)
+install.packages("knitr")
+library(knitr)
+## "eshiny" package for creating interactive web applications which can be
+## embedded on web pages
